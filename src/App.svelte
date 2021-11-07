c6b1699578fa30690a20cecdaa5eb4ae16fc8bf4
smash009 <smash009@naver.com>
2021-11-07 13:26:33 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	let count = 0;
+	function increase() {
+		count++;
+	}
+</script>
+
+<main>
+	<h1>Simple Counter</h1>
+	<p>count : { count }</p>
+	<button on:click="{ increase }">1 Add Count</button>
+</main>
