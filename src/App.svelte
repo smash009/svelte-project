464efd186b4a3b43ef89390f33cae68f0b4bdae8
smash009 <smash009@naver.com>
2021-11-07 13:43:23 +0900

    m

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script>
 	let count = 0;
+	
 	function increase() {
 		count++;
+	}
+	
+	let array = [ 1, 2, 3, 4 ];
+
+	function addItem() {
+		array.push( array.length + 1 );
+		console.log( array );
 	}
 </script>
 
 <main>
 	<h1>Simple Counter</h1>
 	<p>count : { count }</p>
-	<button on:click="{ increase }">1 Add Count</button>
+	<button on:click="{ increase }">Add Count</button>
+	
+	<p>list : { JSON.stringify( array ) }</p>
+	<button on:click="{ addItem }">Add Item to List</button>
 </main>
